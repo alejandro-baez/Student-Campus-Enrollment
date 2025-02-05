5b3c5c2c5f4365910be0a84ccea6530efbf19ee6
Alejandro <89477369+alejandro-baez@users.noreply.github.com>
2025-02-05 00:18:04 -0500

    Added API proxy in vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://backend:80", // Backend service inside Docker
+        changeOrigin: true, // Changes the origin of the request
+        secure: false, // Allows HTTP connections
+      },
+    },
+  },
 })
