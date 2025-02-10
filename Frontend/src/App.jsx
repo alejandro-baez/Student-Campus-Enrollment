4029bcae5729f58a80e3e67fc3fb31b29344538c
Alejandro <89477369+alejandro-baez@users.noreply.github.com>
2025-02-10 00:07:25 -0500

    Created initial footer,homepage, and navbar. Added redux store

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,18 @@
-
+import React from 'react'
+import {Route,Routes} from 'react-router-dom'
+import Homepage from './components/Homepage'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   return(
-    <h1>Hello</h1>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+      </Routes>
+      <Footer/>
+    </div>
   )
 
 }
