0dd97189ae3d8e8faad0216999c0ad9657649738
Alejandro <89477369+alejandro-baez@users.noreply.github.com>
2025-02-09 20:06:49 -0500

    Created a one to many relationship between students and campus

--- Backend/models.py
+++ Backend/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy.orm import mapped_column, Mapped, DeclarativeBase
-from sqlalchemy import text
-from typing import Optional
+from sqlalchemy.orm import mapped_column, Mapped, DeclarativeBase,relationship
+from sqlalchemy import ForeignKey
+from typing import Optional,List
+
 
 class Base(DeclarativeBase):
     pass
@@ -16,6 +17,9 @@ class Student(Base):
     imageUrl: Mapped[Optional[str]] = mapped_column(nullable=False,default="https://d2jyir0m79gs60.cloudfront.net/news/images/successful-college-student-lg.png")
     gpa: Mapped[float]
 
+    campus_id: Mapped[int] = mapped_column(ForeignKey('campuses.id'))
+    campus: Mapped['Campus'] = relationship(back_populates='students')
+
 
 class Campus(Base):
     __tablename__ = "campuses"
@@ -25,3 +29,5 @@ class Campus(Base):
     imageUrl: Mapped[Optional[str]] = mapped_column(nullable=False, default="https://www.cuny.edu/wp-content/uploads/sites/4/page-assets/home-preview/colleges/layout/City-College-Photo-Request-2017_48_CarlosParker_HarrisHall.jpg")
     address: Mapped[str] = mapped_column(nullable=False)
     description: Mapped[str]
+
+    students: Mapped[List["Student"]] = relationship(back_populates='campus')
