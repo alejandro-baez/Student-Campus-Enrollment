4ddf620dc9be7ab35523b0ba164dea9899ee7206
Alejandro <89477369+alejandro-baez@users.noreply.github.com>
2025-02-13 00:09:16 -0500

    First draft of welcome page

--- Frontend/src/components/Homepage.jsx
+++ Frontend/src/components/Homepage.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Banner from '../assets/Banner.png'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <section className='flex section-container mt-[5%]'>
+        {/* left side */}
+        <div className='flex flex-col mt-[5%]'>
+            <div className='font-bold text-3xl'>"Education Is The Most Powerful Weapon Which You Can Use To Change The World"</div>
+            <span className='flex justify-end mt-3 px-1.5 text-xl'>-Nelson Mandela</span>
+        </div>
+
+        {/* right side */}
+        <div>
+            <img src={Banner} alt="collage of education related materials" className='w-300' />
+        </div>
+    </section>
   )
 }
 
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -4,21 +4,6 @@ import Logo from '../assets/grad-cap.png'
 
 const Navbar = () => {
   return (
-    // <div id='nav' className='flex w-full p-6 h-24'  >
-    //     <div className='w-3/4'>
-    //         <Link to='/'>
-    //             <div className='w-full text-3xl font-bold text-blue-800 h-full flex items-center'>ACME Inc.</div>
-    //         </Link>
-    //     </div>
-    //     <div className='flex gap-3 text-lg w-1/4 justify-center items-center h-full'>
-    //         <Link to='/campuses' >
-    //             <div>Campuses</div>
-    //         </Link>
-    //         <Link to ='/students'>
-    //             <div>Students</div>
-    //         </Link>
-    //     </div>
-    // </div>
     <header className='max-w-[1440px] container mx-auto px-4 py-5 flex justify-between border-b-2 sticky top-0 '>
         {/* logo and name */}
         <div >
