29f5cacc8f45e25b4e32caf6ae6154d331b88239
Alejandro <89477369+alejandro-baez@users.noreply.github.com>
2025-02-13 14:13:10 -0500

    Fixed footer design

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -7,6 +7,21 @@ const Footer = () => {
   return (
     <footer className='mt-auto bg-gray-300 h-40 w-full'>
         <div className='section-container flex justify-around'>
+            
+            <div className='flex w-[75%]'>
+                <div className='font-semibold flex items-center'>Useful Links:</div>
+                <ul className='flex items-center'>
+                    <Link to='/' >
+                        <li className='hover:text-white w-20 flex justify-center'>Welcome</li>
+                    </Link>
+                    <Link to='/campuses'>
+                        <li className='hover:text-white w-20 flex justify-center'>Campuses</li>
+                    </Link>
+                    <Link to='/students'>
+                        <li className='hover:text-white w-20 flex justify-center'>Students</li>
+                    </Link>
+                </ul>
+            </div>
 
             <div className='flex flex-col'>
                 <div className='font-semibold'>Connect on social media</div>
@@ -17,23 +32,8 @@ const Footer = () => {
                     <img src={Linkedn} alt="Linkedn logo" className='w-11' />
                 </div>
             </div>
-            <div>
-                <div className='font-semibold'>Useful Links</div>
-                <ul className='flex flex-col items-center space-y-2 pt-2 '>
-                    <Link to='/'>
-                        <li className='hover:text-white'>Welcome</li>
-                    </Link>
-                    <Link to='/campuses'>
-                        <li className='hover:text-white'>Campuses</li>
-                    </Link>
-                    <Link to='/students'>
-                        <li className='hover:text-white'>Students</li>
-                    </Link>
-                </ul>
-            </div>
 
         </div>
-
     </footer>
   )
 }
