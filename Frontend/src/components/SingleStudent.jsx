6a20905eafd2efe24238e488029920e462b70f17
Alejandro <89477369+alejandro-baez@users.noreply.github.com>
2025-02-15 19:28:05 -0500

    Ensuring that all information is loaded on singleStudent using async await

--- Frontend/src/components/SingleStudent.jsx
+++ Frontend/src/components/SingleStudent.jsx
@@ -2,20 +2,48 @@ import React,{useEffect,useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { fetchSingleStudent } from '../features/singleStudentSlice'
+import { fetchAllCampuses } from '../features/campusesSlice'
+import { Link } from 'react-router-dom'
 
 const SingleStudent = () => {
+    const [first_name, setFirstName] = useState("");
+    const [last_name, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [gpa,setGPA] = useState('')
+    const [campus_id, setCampusId] = useState("");
+    const [loading,setLoading] = useState(true)
+
     const {id} = useParams()
     const dispatch = useDispatch()
     const {student} = useSelector(state => state.singleStudent.singleStudent)
+    console.log(student)
     
+    const campuses = useSelector((state) => state.campuses.campuses);
+    const studentCampus = campuses.filter((campus) => campus.id == student.campus_id);
+    console.log(studentCampus)
 
     useEffect(()=>{
-        dispatch(fetchSingleStudent(id))
+        const fetchData = async () =>{
+            setLoading(true)
+            await dispatch(fetchSingleStudent(id))
+            await dispatch(fetchAllCampuses())
+            setLoading(false)
+        }
+        fetchData()
     },[dispatch])
+
+    if (loading || !student) {
+        return <div>Loading...</div>; 
+    }
+
 
 
   return (
-    <div>SingleStudent</div>
+    <section>
+        {/* student*/}
+
+        {/* update form */}
+    </section>
   )
 }
 
